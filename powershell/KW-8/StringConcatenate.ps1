d194a8169e194d71c127d5b8e4f04ef937c0a5db
unknown <anna.nell@noseryoung.com>
2024-02-28 14:20:43 +0100

    Mit Nina angeschaut und gelöst

--- powershell/KW-8/StringConcatenate.ps1
+++ powershell/KW-8/StringConcatenate.ps1
@@ -1,36 +1,39 @@
-$wordList = Get-Content -Path "C:\Users\Anna\Downloads\wordlist.txt"
-
-$usedWords = @()
-
-while ($true) {
-    
-
-    $startwort = $wordList[$startWordIndex]
-
-    
-    if ($usedWords -contains $startwort) {
-        continue
+$start_word = "Hello"
+Write-Host "Start word: $start_word"
+ 
+$valid_words = Get-Content -Path "C:\Users\ninab\BLJ2024NiBis\Projects\PowerShell\2024\KW09\StringConcatenate\wordlist.txt"
+ 
+function Is-Valid {
+    param(
+        [string]$word,
+        [char]$last_letter
+    )
+    $word = $word.Trim()
+    if ($valid_words -like "*$word*" -and $word[0] -eq $last_letter) {
+        return $true
     }
-    
-   
-    $usedWords += $startwort
-
-    $nextWort = Read-Host "Enter another word (or press Ctrl+C to exit)"
-    
-    $startwortEndbuchstabe = $startwort[-1]
-
-    if ($nextWort -eq "") {
-        Write-Host "No further word entered. Game over"
-        break
-    }
-    elseif ($nextWort -in $wordList -and $nextWort[0] -eq $startwortEndbuchstabe) {
-        
-        $startwort += "$nextsWort"
-        Write-Host $startwort
-    }
-    else {
-        Write-Host "The word entered does not match or is not valid. Game over"
-        break
+    return $false
+}
+ 
+function String-Concatenate {
+    param(
+        [string]$start_word
+    )
+    $last_letter = $start_word[-1]
+    Write-Host "Last letter of the start word: $last_letter"
+ 
+    while ($true) {
+        $new_word = Read-Host "Enter another word (or end with CTRL+C)"
+        if (Is-Valid -word $new_word -last_letter $last_letter) {
+            Write-Host "Valid word: $new_word"
+            $start_word += " $new_word"
+            Write-Host "Current string: $start_word"
+            $last_letter = $new_word[-1]
+        }
+        else {
+            Write-Host "The word doesn't exist or has already been used."
+        }
     }
 }
-
+ 
+String-Concatenate -start_word $start_word
