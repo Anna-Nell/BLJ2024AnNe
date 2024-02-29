7a6682cc7f150965a9172f0dcadb00254940def0
unknown <anna.nell@noseryoung.com>
2024-02-29 14:13:41 +0100

    Daran das Teams in Get-Prozess zu finden

---
+++ powershell/KW-9/Pipeline.ps1
@@ -0,0 +1,25 @@
+# Retrieve a list of all the members (methods and properties) of the Get-ChildItem cmdlet.
+# Use Get-ChildItem to retrieve a list of the files in the current directory and the Get-Member cmdlet to retrieve a list of all the members
+Get-Command Get-ChildItem | Get-Member
+
+# Retrieve a list of all the files in the current directory and then sort them ascending by size. 
+# Use the Get-ChildItem cmdlet to retrieve the list of files and the Sort-Object cmdlet to sort them by size.
+Get-ChildItem | Where-Object { -not $_.PSIsContainer } | Sort-Object Length
+
+# Retrieve a list of all the processes currently running on the computer and then select the processes that have a high CPU usage (>50%). 
+# Use the Get-Process cmdlet to retrieve the list of processes and the Where-Object cmdlet to select only the processes with a high CPU usage.
+Get-Process | Where-Object { $_.CPU -gt 50 }
+
+# Retrieve a list of all the services and select only the processes which are running. Display only the service name and the service description.
+# Use the Get-Service cmdlet to retrieve a list of all services, the Where-Object cmdlet to select running processes and the Select-Object cmdlet to display  the service name and the service description.
+Get-Service | Where-Object { $_.Status -eq "Running" } | Select-Object DisplayName, Description
+
+# Retrieve a list of all the processes on the computer and then stop the processes that contain "Team" in the name. 
+
+
+# Retrieve a list of all the local users username and last logon time and sort them by their last logon time. Don't list users, which have never logged on.
+
+
+# Retrieve a list of all the files in the current directory and then select the files that have been modified in the past week. Sort them by their modification date.
+
+
--- powershell/KW-9/Sort-Files.ps1
+++ powershell/KW-9/Sort-Files.ps1
@@ -4,30 +4,31 @@ New-Item -ItemType Directory -Path ".\TestingPurpose\SubFolder2" -Force
 # Erstellen Sie einige Testdateien in diesen Ordnern:
 # 	TypeATest1.txt, TypeATest2.txt ... TypeATest50.txt in SubFolder1
 # 	TypeBTest51.txt, TypeBTest52.txt ... TypeBTest100 in SubFolder2
-ForEach-Object {
-    if($_ % 2 -eq 0) {
-       New-Item -Path ".\TestingPurpose\SubFolder1\TypeATest$_" -ItemType -Force
+1..100 | ForEach-Object {
+   if($_ % 2 -eq 0) {
+       New-Item -Path ".\TestingPurpose\SubFolder1\TypeATest$_.txt" -ItemType File -Force
     }
     else {
-        New-Item -Path ".\TestingPurpose\SubFolder2\TypeBTest$_" -ItemType -Force
+        New-Item -Path ".\TestingPurpose\SubFolder2\TypeBTest$_.txt" -ItemType File -Force
     }
 
 }
 # Verschiebe alle Dateien, die eine ungerade Zahl im Namen haben, in SubFolder2. (Modulo)
-Get-ChildItem -Path ".\TestingPurpose\SubFolder1" | Where-Object {$_.Name -match '\d+' -and [int]$Matches[0] % 2 -ne 0} Move-Item -direstory ".\Testing\SubFolder2"
+Get-ChildItem -Path ".\TestingPurpose\SubFolder2" | Where-Object { $_.Name -match '\d+' -and [int]$Matches[0] % 2 -ne 0} Move-Item -Destination ".\Testing\SubFolder2"
 
 # Verschiebe alle Dateien, die eine gerade Zahl im Namen haben, in SubFolder1.
-et-ChildItem -Path ".\TestingPurpose\SubFolder1" | Where-Object {$_.Name -match '\d+' -and [int]$Matches[0] % 2 -eq 0} Move-Item -direstory ".\Testing\SubFolder1"
+Get-ChildItem -Path ".\TestingPurpose\SubFolder1" | Where-Object { $_.Name -match '\d+' -and [int]$Matches[0] % 2 -eq 0} Move-Item -Destination ".\Testing\SubFolder1"
 
 # Benenne den Ordner SubFolder1 in EvenFilesContainer und SubFolder2 in OddFilesContainer um.
 Rename-Item -Path ".\TestingPurpose\SubFolder1" -NewName "EvenfilesContainer" -Force
 Rename-Item -Path ".\TestingPurpose\SubFolder2" -NewName "OddFilesContainer" -Force
-# Erstellen Sie eine Liste aller Dateien, die sich derzeit im Ordner TestingPurpose befinden.
-$listfiles = Get-ChildItem -path ".\TestingPurpose" -Recurse -File | Select-Object FullName
 
-Write-Host "Ab $(Get-Date -Format 'yyyyMMdd HH:mm') befinden sich folgende Dateien in TestingPurpose:"
+# Erstellen Sie eine Liste aller Dateien, die sich derzeit im Ordner TestingPurpose befinden.
+$filesList = Get-ChildItem -path ".\TestingPurpose" -Recurse -File | Select-Object FullName
+
+Write-Host "Ab $(Get-Date -Format 'dd.MM.yyyy HH:mm') befinden sich folgende Dateien in TestingPurpose:"
 $filesList | ForEach-Object { Write-Host $_.FullName }
 
 # Löschen Sie alle Dateien, die mit TypeA beginnen
-Remove-Item -Path ".\TestingPurpose\EvenFilesContainer\TypeA*" -Force
+Remove-Item -Path ".\TestingPurpose\EvenFilesContainer\TypeA*.txt" -Force
 
