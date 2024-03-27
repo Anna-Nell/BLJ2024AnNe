d4db539976150fd43c75abfffb2e4ce52c9f6c6e
unknown <anna.nell@noseryoung.com>
2024-03-27 13:03:52 +0100

    erstellung von den GUI eingabe kästchen

--- powershell/KW-13/ADuser.ps1
+++ powershell/KW-13/ADuser.ps1
@@ -30,3 +30,25 @@ function Create-Button {
     $button.Add_Click($OnClick)
     return $button
 }
+function Generate-Password {
+    $length = 12
+    $characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-="
+    $password = -join ((1..$length) | ForEach-Object { Get-Random -InputObject $characters })
+    return $password
+}
+
+
+$window = New-Object System.Windows.Forms.Form
+$window.text = "Active Directory Benutzer erstellen"
+$window.Width = 600
+$window.Height = 400
+
+# Label für Benutzerinformationen
+$Label_firstName = Create-Label -text "Vorname:" -x 20 -y 20 -width 100 -height 20
+$Label_lastName = Create-Label -text "Nachname:" -x 20 -y 50 -width 100 -height 20
+$Label_description = Create-Label -text "Description:" -x 20 -y 80 -width 100 -height 20
+$Label_office = Create-Label -text "Office:" -x 20 -y 110 -width 100 -height 20
+$Label_phone = Create-Label -text "Telefonnummer:" -x 20 -y 140 -width 100 -height 20
+$Label_jobTitle = Create-Label -text "Job Titel:" -x 20 -y 170 -width 100 -height 20
+$Label_address = Create-Label -text "Adresse:" -x 20 -y 200 -width 100 -height 20 
+
